52cbd1e93a3fd351a4936a77578a740f9663e1c7
Guilherme <guilhermealles@users.noreply.github.com>
2016-06-22 00:41:46 -0300

    results

--- build_and_run.sh
+++ build_and_run.sh
@@ -4,13 +4,13 @@
 make clean
 make
 
-./genetic ./instances/br17.atsp 1800 > outputs/1800s_br17.txt
-./genetic ./instances/ft70.atsp 1800 > outputs/1800s_ft70.txt
-./genetic ./instances/ftv33.atsp 1800 > outputs/1800s_ftv33.txt
-./genetic ./instances/ftv44.atsp 1800 > outputs/1800s_ftv44.txt
-./genetic ./instances/ftv55.atsp 1800 > outputs/1800s_ftv55.txt
-./genetic ./instances/ftv170.atsp 1800 > outputs/1800s_ftv170.txt
-./genetic ./instances/kro124p.atsp 1800 > outputs/1800s_kro124p.txt
-./genetic ./instances/rbg323.atsp 1800 > outputs/1800s_rbg323.txt
-./genetic ./instances/rbg358.atsp 1800 > outputs/1800s_rbg358.txt
-./genetic ./instances/rbg403.atsp 1800 > outputs/1800s_rbg403.txt
+./genetic ./instances/br17.atsp 120 > outputs/a_120s_br17.txt &
+./genetic ./instances/ft70.atsp 120 > outputs/a_120s_ft70.txt &
+./genetic ./instances/ftv33.atsp 120 > outputs/a_120s_ftv33.txt &
+./genetic ./instances/ftv44.atsp 120 > outputs/a_120s_ftv44.txt &
+./genetic ./instances/ftv55.atsp 120 > outputs/a_120s_ftv55.txt &
+./genetic ./instances/ftv170.atsp 120 > outputs/a_120s_ftv170.txt &
+./genetic ./instances/kro124p.atsp 120 > outputs/a_120s_kro124p.txt &
+./genetic ./instances/rbg323.atsp 120 > outputs/a_120s_rbg323.txt &
+./genetic ./instances/rbg358.atsp 120 > outputs/a_120s_rbg358.txt &
+./genetic ./instances/rbg403.atsp 120 > outputs/a_120s_rbg403.txt &
